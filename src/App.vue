4ca1646fc079791fbf50565482b2020f7e16d992
yjx <578755236@qq.com>
2020-10-02 23:37:20 +0800

    Vue 定义组件方式

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,68 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <h1>大白菜</h1>
-      <!-- 根路由出口 -->
-      <router-link/>
-    </div>
+    <h1>大白菜</h1>
+    <!-- 根路由出口 -->
+    <button @click="test">测试</button>
     <router-view/>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component
+export default class App extends Vue {
+  name = '大白菜'
+  age = '18'
+  userInfo = {
+    title: '1',
+    age: 20
+  }
+
+  test () {
+    console.log(this.age)
+  }
+}
+</script>
+
+<!--<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      name: '大白菜',
+      age: '18',
+      userInfo: {
+        title: '1',
+        age: 20
+      }
+    }
+  },
+  methods: {
+  }
+})
+</script>-->
+
+<!-- <script>
+// 1、编辑器给的类型提示
+// 2、TypeScript 编译期间的类型验证
+export default {
+  data () {
+    return {
+      name: '大白菜',
+      age: '18',
+      userInfo: {
+        title: '1',
+        age: 20
+      }
+    }
+  },
+  methods: {
+    test () {
+      // this.data.
+      this.age.abc()
+    }
+  }
+}
+</script>-->
 
 <style lang="scss" scoped></style>
