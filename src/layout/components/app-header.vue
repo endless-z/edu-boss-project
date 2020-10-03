5c457c1f4a183177dcd80944b3485f505abbd325
yjx <578755236@qq.com>
2020-10-03 23:32:21 +0800

    处理头部

---
+++ src/layout/components/app-header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        <el-avatar
+        shape="square"
+        :size="30"
+        src="https://avatars1.githubusercontent.com/u/20332212?s=460&u=3b14423cb13b3c66630b7c93fa4352c846107a1e&v=4">
+        </el-avatar><i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>用户ID</el-dropdown-item>
+        <el-dropdown-item disabled>退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'AppHeader'
+})
+</script>
+
+<style lang="scss" scoped>
+.header{
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .el-dropdown-link{
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <app-aside />
+    </el-aside>
+    <el-container>
+      <el-header>
+        <app-header />
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AppAside from './components/app-aside.vue'
+import AppHeader from './components/app-header.vue'
+export default Vue.extend({
+  name: 'LayoutIndex',
+  components: {
+    AppAside,
+    AppHeader
+  }
+})
+</script>
+<style lang="scss" scoped>
+.el-container{
+  min-height: 100vh;
+}
+ .el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  line-height: 200px;
+}
+.el-header, .el-footer {
+  background-color: #ffffff;
+}
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  line-height: 160px;
+}
+</style>
