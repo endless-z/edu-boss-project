608e6111d3d5196cfb582e28f95a1fb4083ff05b
yjx <578755236@qq.com>
2020-10-04 22:09:03 +0800

    使用请求拦截器统一设置Token

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import store from '@/store'
 const request = axios.create({
   // 配置选项
   // baseURL
@@ -7,6 +7,17 @@ const request = axios.create({
 })
 
 // 请求拦截器
-
+request.interceptors.request.use(function (config) {
+  console.log('接口请求进来了', config)
+  // 通过改写 config 配置信息来实现业务功能
+  const { user } = store.state
+  if (user && user.access_token) {
+    config.headers.Authorization = user.access_token
+  }
+  // 注意: 这里一定要返回 config， 否则请求就发不出去了
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
 // 响应拦截器
 export default request
