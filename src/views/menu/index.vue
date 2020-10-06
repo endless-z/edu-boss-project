001707454575a21652a330ff713686066e64815a
yjx <578755236@qq.com>
2020-10-06 23:59:47 +0800

    菜单

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -36,9 +36,14 @@
           <el-input v-model="form.href"></el-input>
         </el-form-item>
         <el-form-item label="上级菜单">
-          <el-select v-model="form.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
+          <el-select v-model="form.parentId" placeholder="请选择上级菜单">
+            <el-option :value="-1" label="无上级菜单"></el-option>
+            <el-option
+            :label="item.name"
+            :value="item.id"
+            v-for="item in parentMenuList"
+            :key="item.id"
+            ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="描述">
@@ -68,7 +73,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { createOrUpdateMenu } from '@/services/menu'
+import { createOrUpdateMenu, getEditMenuInfo } from '@/services/menu'
 
 export default Vue.extend({
   name: 'Menu',
@@ -100,10 +105,20 @@ export default Vue.extend({
         orderNum: 0,
         description: '123',
         shown: false
-      }
+      },
+      parentMenuList: [] // 父级菜单列表
     }
   },
+  created () {
+    this.loadMenuInfo()
+  },
   methods: {
+    async loadMenuInfo () {
+      const { data } = await getEditMenuInfo()
+      if (data.code === '000000') {
+        this.parentMenuList = data.data.parentMenuList
+      }
+    },
     async onSubmit () {
       console.log('submit!')
       // 1、表单验证
