1dc4eecfe7c9ca7e88e329ef50809fe0b755ad92
yjx <578755236@qq.com>
2020-10-09 20:37:38 +0800

    编辑菜单

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -5,130 +5,71 @@
     </div>
 
     <el-table
-      :data="tableData"
+      :data="menus"
       border
       style="width: 100%">
       <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
+        type="index"
+        label="编号">
       </el-table-column>
       <el-table-column
         prop="name"
-        label="姓名"
-        width="180">
+        label="菜单名称">
       </el-table-column>
       <el-table-column
-        prop="address"
-        label="地址">
+        prop="level"
+        label="菜单级数">
+      </el-table-column>
+      <el-table-column
+        prop="icon"
+        label="前端图标">
+      </el-table-column>
+      <el-table-column
+        prop="orderNum"
+        label="排序">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.row)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.row)">删除</el-button>
+        </template>
       </el-table-column>
     </el-table>
-
-    <el-dialog
-      title="添加菜单"
-      :visible.sync="dialogFormVisible"
-      width="30%">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="菜单名称">
-          <el-input v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item label="菜单路径">
-          <el-input v-model="form.href"></el-input>
-        </el-form-item>
-        <el-form-item label="上级菜单">
-          <el-select v-model="form.parentId" placeholder="请选择上级菜单">
-            <el-option :value="-1" label="无上级菜单"></el-option>
-            <el-option
-            :label="item.name"
-            :value="item.id"
-            v-for="item in parentMenuList"
-            :key="item.id"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="描述">
-          <el-input v-model="form.description"></el-input>
-        </el-form-item>
-        <el-form-item label="图标">
-          <el-input v-model="form.icon"></el-input>
-        </el-form-item>
-        <el-form-item label="是否显示">
-          <el-radio-group v-model="form.shown">
-            <el-radio :label="true">是</el-radio>
-            <el-radio :label="false">否</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="排序">
-          <el-input-number v-model="form.orderNum" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
-        </el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="onSubmit">确 定</el-button>
-      </span>
-    </el-dialog>
-
+    <create-or-edit :isEdit="isEdit" :dialogFormVisible="dialogFormVisible" :id="id"></create-or-edit>
   </div>
 </template>
 
 <script lang="ts">
+import CreateOrEdit from './components/CreateOrEdit.vue'
 import Vue from 'vue'
-import { createOrUpdateMenu, getEditMenuInfo } from '@/services/menu'
+import { getAllMenus, deleteMenu } from '@/services/menu'
 
 export default Vue.extend({
   name: 'Menu',
+  components: {
+    CreateOrEdit
+  },
   data () {
     return {
       dialogFormVisible: false,
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
-      form: {
-        parentId: -1, // 没有上级
-        name: '123',
-        href: '123',
-        icon: '123',
-        orderNum: 0,
-        description: '123',
-        shown: false
-      },
-      parentMenuList: [] // 父级菜单列表
+      menus: [],
+      id: -1,
+      isEdit: false
     }
   },
   created () {
-    this.loadMenuInfo()
+    this.loadAllMenus()
   },
   methods: {
-    async loadMenuInfo () {
-      const { data } = await getEditMenuInfo()
+    async loadAllMenus () {
+      const { data } = await getAllMenus()
       if (data.code === '000000') {
-        this.parentMenuList = data.data.parentMenuList
-      }
-    },
-    async onSubmit () {
-      console.log('submit!')
-      // 1、表单验证
-      // 2、验证通过， 通过表单
-      const { data } = await createOrUpdateMenu(this.form)
-      if (data.code === '000000') {
-        this.$message.success('提交成功')
-        this.dialogFormVisible = false
-      } else {
-        this.$message.error('提交失败')
+        this.menus = data.data
       }
     },
     handleCreate () {
@@ -136,6 +77,27 @@ export default Vue.extend({
     },
     handleChange () {
       console.log('change')
+    },
+    handleEdit (item: any) {
+      this.dialogFormVisible = true
+      this.isEdit = true
+      console.log(item.id)
+      this.id = item.id
+      console.log('edit', item)
+    },
+    handleDelete (item: any) {
+      console.log('delet', item)
+      this.$confirm('确认删除吗?', '删除提示', {})
+        .then(async () => {
+          const { data } = await deleteMenu(item.id)
+          if (data.code === '000000') {
+            this.$message.success('删除成功')
+            this.loadAllMenus()
+          }
+        }).catch(err => {
+          console.log(err)
+          this.$message.info('已取消删除')
+        })
     }
   }
 })
